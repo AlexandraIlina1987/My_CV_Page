1a1924572b18b9ce28898206f796807eda639b50
Alexandra Ilina <test@test.ru>
2026-06-16 15:07:55 +0200

    add Connect With Me button to main page with custom styling

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,12 @@
 import { Layout } from "../../components/Layout/Layout"
+import { Button } from "../../components/Button/Button"
+import styles from "../../components/Button/Button.module.css"
 
 export const MainPage = () => {
     return (
         <Layout>
             <h1>Main Page</h1>
+            <Button onClick={() => {}} label="Connect With Me" className={styles.buttonConnect } />
         </Layout>
     )
 }
